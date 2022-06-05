2e563a2cf09b6adbcf48dde111f541cd2efad55a
rtek1000 <rh.silicon@gmail.com>
2022-06-05 19:12:50 -0300

    Update

---
+++ Software/Arduino_JiuJitsu_Panel/Arduino_JiuJitsu_Panel.ino
@@ -0,0 +1,17 @@
+// https://github.com/rtek1000/LED_Panel_P10_Fight_Timer_DMD2_HUB12
+
+#include <avr/wdt.h> // Arduino IDE
+
+#include <SPI.h> // Arduino IDE
+
+#include "DMD2.h" // https://github.com/freetronics/DMD2/blob/master/DMD2.h
+
+#include "fonts/SystemFont5x7.h" // DMD2 lib
+
+#include <EEPROM.h> // Arduino IDE
+
+#include <Wire.h> // Arduino IDE
+
+#include "RTClib.h" // https://github.com/adafruit/RTClib/blob/master/src/RTClib.h
+
+#include <SoftwareSerial.h> // Arduino IDE
